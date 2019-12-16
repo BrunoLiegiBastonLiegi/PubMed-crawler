014f5636d3d0fc0c504c592e912e9a8094de0b32
andrea <andreapapaluca@gmail.com>
2019-12-16 10:33:27 +0100

    modifiche varie

--- crawler.py
+++ crawler.py
@@ -2,12 +2,14 @@ import requests as req
 from bs4 import BeautifulSoup
 
 #params passed to esearch
-payload = {'db':'pubmed', 'term':'ageing OR longevity'}
+payload = {'db':'pubmed', 'term':'ageing OR longevity', 'retmax':100}
 
 #esearch
+print('--> Searching Pubmed')
 r = req.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi', params=payload)
 
 #parsing Ids
+print('--> Retrieving Ids')
 soup = BeautifulSoup(r.text, "xml")
 Id = soup.Id
 
@@ -16,29 +18,30 @@ idlist = []
 while Id != None:
     idlist.append(int(Id.text))
     Id = Id.find_next('Id')
-    
-#params passed to efetch
-payload = {'db':'pubmed', 'id':idlist, 'retmode':'xml'}
 
-#efetch
-r = req.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi', params=payload)
 
-#parsing
-soup = BeautifulSoup(r.text, "xml")
-
-article = soup.find('PubmedArticle')
-line = []
 f = open('articles.json','w')
-
 
 def write_line(f, line):
     return f.write('{\"index\":{\"_id\":\"'+str(line[0])+'\"}}\n'+'{\"PMID\":'+str(line[0])+',\"Title\":\"'+str(line[1])+'\",\"Abstract\":\"'+str(line[2]).replace('\n',' ').replace('"',' ')+'\"}\n')
 
-
+print('--> Retrieving abstratcs and titles')
 for i in range(len(idlist)):
-    line.append([article.PMID.text, article.ArticleTitle.text, article.Abstract.text])
-    write_line(f, line[i])
-    article = article.find_next('PubmedArticle')
+    
+    #params passed to efetch
+    payload = {'db':'pubmed', 'id':idlist[i], 'retmode':'xml'}
+
+    #efetch
+    r = req.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi', params=payload)
+
+    #parsing
+    soup = BeautifulSoup(r.text, "xml")
+
+    if soup.Abstract != None:
+        line = [soup.PMID.text, soup.ArticleTitle.text, soup.Abstract.text]
+    else:
+        line = [soup.PMID.text, soup.ArticleTitle.text, 'Abstract not avaliable']
+    write_line(f, line)
 
 
 
