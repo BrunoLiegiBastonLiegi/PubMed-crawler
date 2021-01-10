0c283b08fa88d96c2224b00fcc3200053038ee4a
BrunoLiegiBastonLiegi <45011234+BrunoLiegiBastonLiegi@users.noreply.github.com>
2021-01-10 18:47:03 +0100

    Add files via upload

--- causal_graph/ke.py
+++ causal_graph/ke.py
@@ -31,24 +31,30 @@ class Pipeline(torch.nn.Module):
         self.h_lin = torch.nn.Linear(self.bert_dim + self.ner_dim + self.ned_dim, self.ht_dim)
         self.t_lin = torch.nn.Linear(self.bert_dim + self.ner_dim + self.ned_dim, self.ht_dim)
 
+        # RE
+        self.re_dim = 128  # dimension of RE embedding 
+        self.re_bil = torch.nn.Bilinear(self.ht_dim, self.ht_dim, self.re_dim)
+        self.re_lin = torch.nn.Linear(2*self.ht_dim, self.re_dim, bias=False)  # we need only one bias, we can decide to
+                                                                               # switch off either the linear or bilinear bias
+
     def forward(self, x):
         x = self.BERT(x)
+        print('### BERT encoding:\n', x.shape)
         ner = self.NER(x)                                           # this is the output of the linear layer, should we use this as
-        x = torch.cat((x, self.sm(ner)), 1)                                  # as embedding or rather the softmax of this?
+        x = torch.cat((x, self.sm(ner)), 1)                         # as embedding or rather the softmax of this?
+        print('### NER encoding:\n', x.shape)
         
         # remove non-entity tokens, before this we need to merge multi-token entities
-        tmp = []
-        for xx in x:
-            if int(torch.argmax(xx[-self.ner_dim:])) != self.ner_dim - 1 :
-                tmp.append(xx)
-        x = torch.stack(tmp, dim=0)
-
+        x = self.Entity_filter(x)
+        print('### Entities found:\n', x.shape)
         ned = self.NED(x)
         x = torch.cat((x, ned), 1)
-        x, y = self.HeadTail(x)
+        print('### NED encoding:\n', x.shape)
+        re = self.RE(x)
+        print('### RE encoding:\n', re.shape)
+        return ner, ned, re
 
-        # building candidate pairs
-        for 
+
         
 
     def BERT(self, x):
@@ -65,14 +71,46 @@ class Pipeline(torch.nn.Module):
     def NER(self, x):
         return self.ner_lin(x)
 
+    def Entity_filter(self, x):
+        tmp = []
+        for xx in x:
+            if int(torch.argmax(xx[-self.ner_dim:])) != self.ner_dim - 1 :
+                tmp.append(xx)
+        return torch.stack(tmp, dim=0)
+
     def NED(self, x):
-        return self.ned_lin(x) # it's missing the dot product in the graph embedding space, the idea would be find the closest
-                               # concepts in embedding space and then return the closest in a greedy approach, or the closests
-                               # with beam search
+        return self.ned_lin(x) # it's missing the dot product in the graph embedding space, the idea would be to find the closest
+                               # concepts in embedding space and then return the closest in a greedy approach, or the closest ones
+                               # with beam search. We also need to decide if it's better to use the predicted graph embedding of
+                               # the concept or to map the predicted embedding to the corresponding true graph embedding and then 
+                               # use this for RE.
 
     def HeadTail(self, x):
         h = self.h_lin(x)
         t = self.t_lin(x)
-        return (h,t)
+        # Building candidate pairs
+        head = torch.stack([ h for i in range(x.shape[0])], dim=1).view(x.shape[0]**2, h.shape[1])    # Combining all possible heads
+        tail = torch.stack([ t for i in range(x.shape[0])]).view(x.shape[0]**2, t.shape[1])           # with every possible tail
+        return (head,tail)
 
     def Biaffine(self, x, y):
+        return self.re_bil(x,y) + self.re_lin(torch.cat((x,y), dim=1))
+
+    def RE(self, x):
+        x, y = self.HeadTail(x)
+        return self.Biaffine(x,y)
+
+
+
+
+
+
+
+sents = ['Obesity is a cause for Diabetes Mellitus', 'Diabetes type 2a is common in obese people']
+
+bert = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract"
+model = Pipeline(bert)
+#[ print(i.shape) for i in model(sents[0]) ]
+critetion = torch.nn.CrossEntropyLoss()
+model(sents[0])
+model.backward()
